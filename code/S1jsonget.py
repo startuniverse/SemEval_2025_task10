9bfa94a252b71b79640fc7843b7f59e6740a8fc4
startuniverse <144691623+startuniverse@users.noreply.github.com>
2025-02-23 23:19:59 +0800

    Update S1jsonget.py

--- code/S1jsonget.py
+++ code/S1jsonget.py
@@ -32,17 +32,17 @@ def save_json(json_list, output_file):
     with open(output_file, 'w', encoding='utf-8') as file:
         json.dump(json_list, file, indent=4, ensure_ascii=False)
 
-# 文件路径
+
 input_file_path = r"D:\zhuomian\test\PT\subtask-1-entity-mentions.txt"
 output_file_path = 'TESTS1PT.json'
 
-# 读取文件
+
 lines = read_file(input_file_path)
 
-# 转换为 JSON 格式
+
 json_result = convert_to_json(lines)
 
-# 保存为 JSON 文件
+
 save_json(json_result, output_file_path)
 
-print(f"转换完成！JSON 文件已保存到 {output_file_path}")
+print(f"save in{output_file_path}")
